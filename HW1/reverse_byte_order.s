e9fce9a43b61b50f3c15854775cb2c60862563fb
Brician <belizairebrician@gmail.com>
2026-09-14 13:40:37 -0400

    closer

--- HW1/reverse_byte_order.s
+++ HW1/reverse_byte_order.s
@@ -3,24 +3,36 @@
 
 main:
 	#get last byte and move to front
-	andi t3, s2, 0x000000FF
+	lui t3, 0x0000
+	addi t3, t3, 0x0FF
+	and t3, t3, s2
 	slli t3, t3, 0x6
+
 	#get third byte and move to second byte
-	andi t2, s2, 0x0000FF00
+	lui t2, 0x0000F
+	addi t2, t2, 0xF00
+	and t2, t2, s2
 	slli t2, t2, 0x2
+
 	#get second byte and move to third byte
-	andi t1, s2, 0x00FF0000
-	srli t1, t1, 0x2 
+	lui t1, 0x00FF0
+	addi t1, t1, 0x000
+	and t1, t1, s2
+	srli t1, t1, 0x2
+ 
 	#get first byte
-	andi t0, s2, 0xFF000000
+	lui t0, 0xFF000
+	addi t0, t0, 000
+	and t0, t0, s2
 	srli t0, t0, 0x6
 	
 	#add t registers and leave result in s4
 	add s4, t3, t2
 	add s4, s4, t1
 	add s4, s4, t0
+
 	#end program
-	addi a0, x0, x0
+	add a0, x0, x0
 	addi a7, x0, 93
 	ecall
 
